b5c680dc59ef5b5ce17b062bc199a47b54e4813b
Swapnil Joijode <koushikjoijode@gmail.com>
2025-12-30 22:37:42 -0500

    Feature Engineering Seperated

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -10,6 +10,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 import os
 from src.utils import save_object
+from src.components.feature_engineering import initiate_feature_engineering
 
 
 @dataclass
@@ -53,41 +54,12 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e, sys)
 
-    def initiate_feature_engineering(self, df_path: str):
-        try:
-            df = pd.read_csv(df_path)
-            logging.info("Read train and test data completed")
-
-            logging.info("Starting feature engineering")
-            
-            df["join_date"] = pd.to_datetime(df["join_date"])
-            df["tenure_days"] = (pd.Timestamp("2024-12-31") - df["join_date"]).dt.days
-            df["watch_bucket"] = pd.qcut(
-                df["avg_watch_minutes"],
-                q=5,
-                labels=["Very Low", "Low", "Medium", "High", "Very High"]
-            )
-            df["late_bucket"] = pd.cut(
-                df["late_ratio"],
-                bins=[0, 0.05, 0.15, 0.3, 1.0],
-                labels=["No Issues", "Minor", "Moderate", "Severe"]
-            )
-            df["low_usage"] = (df["avg_watch_minutes"] < 60).astype(int)
-            df["payment_issue"] = (df["late_ratio"] > 0.1).astype(int)
-
-
-
-            logging.info("Feature engineering completed")
-
-            # Save the engineered features back to CSV if needed
-            return df
-        except Exception as e:
-            raise CustomException(e, sys)
+    
         
     def initiate_data_transformation(self, train_path: str, test_path: str):
         try:
-            train_df = self.initiate_feature_engineering(train_path)
-            test_df = self.initiate_feature_engineering(test_path)
+            train_df = initiate_feature_engineering(train_path)
+            test_df = initiate_feature_engineering(test_path)
             logging.info("Read train and test data completed")
 
             numericalcolumns = train_df.drop(columns=['churn']).select_dtypes(include=['int64', 'float64']).columns.tolist()
---
+++ src/components/feature_engineering.py
@@ -0,0 +1,38 @@
+import sys
+import pandas as pd
+import os
+import numpy as np
+from src.logger import logging
+from src.exception import CustomException
+
+
+def initiate_feature_engineering(df_path: str):
+        try:
+            df = pd.read_csv(df_path)
+            logging.info("Read train and test data completed")
+
+            logging.info("Starting feature engineering")
+            
+            df["join_date"] = pd.to_datetime(df["join_date"])
+            df["tenure_days"] = (pd.Timestamp("2024-12-31") - df["join_date"]).dt.days
+            df["watch_bucket"] = pd.qcut(
+                df["avg_watch_minutes"],
+                q=5,
+                labels=["Very Low", "Low", "Medium", "High", "Very High"]
+            )
+            df["late_bucket"] = pd.cut(
+                df["late_ratio"],
+                bins=[0, 0.05, 0.15, 0.3, 1.0],
+                labels=["No Issues", "Minor", "Moderate", "Severe"]
+            )
+            df["low_usage"] = (df["avg_watch_minutes"] < 60).astype(int)
+            df["payment_issue"] = (df["late_ratio"] > 0.1).astype(int)
+
+
+
+            logging.info("Feature engineering completed")
+
+            # Save the engineered features back to CSV if needed
+            return df
+        except Exception as e:
+            raise CustomException(e, sys)
